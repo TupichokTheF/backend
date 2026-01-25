540858af5dc6e6b59fd0a6e894cdfe3bf3740cd1
TuichokTheF <165381384+TupichokTheF@users.noreply.github.com>
2026-01-25 16:11:33 +0000

    микро изменения

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 import uvicorn
 from contextlib import asynccontextmanager
 
-from app.api.main import api_router
+from app.api.api_router import api_router
 from app.database import database
 
 @asynccontextmanager
